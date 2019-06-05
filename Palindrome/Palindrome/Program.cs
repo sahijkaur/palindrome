d2dcd3fc1a1c113a143bac362f8714b74ba3967f
sahij kaur <sahijpalkaur@gmail.com>
2019-06-05 09:52:16 -0400

    2

--- Palindrome/Palindrome/Program.cs
+++ Palindrome/Palindrome/Program.cs
@@ -10,27 +10,51 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
-            string s, revs = "";
-            Console.WriteLine(" Enter string");
-            s = Console.ReadLine();
-            for (int i = s.Length - 1; i >= 0; i--) //String Reverse  
-            {
-                revs += s[i].ToString();
-            }
-            if (revs == s) // Checking whether string is palindrome or not  
-            {
-                Console.WriteLine("String is Palindrome \n Entered String Was {0} and reverse string is {1}", s, revs);
-            }
-            else
-            {
-                Console.WriteLine("String is not Palindrome \n Entered String Was {0} and reverse string is {1}", s, revs);
-            }
-            Console.ReadKey();
+            var p = new PlayWithPalindromes();
+            Console.WriteLine(p.Kagaroo());
         }
     }
 
-    class playWithPalindromes
+    class PlayWithPalindromes
     {
+        static string YouWin = "You Win!";
+        public string Kagaroo()
+        {
+            Console.WriteLine("want to play a game?");
+            Console.WriteLine("enter a word");
+            string Word1 = Console.ReadLine();
+            Console.WriteLine("enter another word");
+            string Word2 = Console.ReadLine();
 
+            // parse each word and enter it
+            // letter by letter into an ARRAY
+
+            string[] WordOneArray = new string[5];
+            string[] WordTwoArray = new string[5];
+
+            for (int i = 0; i < Word1.Length; i++)
+            {
+                WordOneArray[i] = Word1.Substring(i, 1);
+            }
+
+            for (int i = 0; i < Word2.Length; i++)
+            {
+                WordTwoArray[i] = Word2.Substring(i, 1);
+            }
+
+            // how to compare 2 arrays?
+            int z = Word2.Length;
+            for (int j = 0; j < WordOneArray.Length; j++)
+            {
+                if (WordOneArray[j] != WordTwoArray[Word2.Length - z--])
+                {
+                 //   Console.WriteLine(WordOneArray[word2.length-z]);
+                    YouWin = "You Lose!";
+                    break;
+                }
+            }
+
+            return YouWin;
+        }
     }
 }
