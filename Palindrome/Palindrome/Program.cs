559f3e38796098f4ae83631de32473dc7ad764c6
sahij kaur <sahijpalkaur@gmail.com>
2019-06-05 08:42:56 -0400

    first commit

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s, revs = "";
+            Console.WriteLine(" Enter string");
+            s = Console.ReadLine();
+            for (int i = s.Length - 1; i >= 0; i--) //String Reverse  
+            {
+                revs += s[i].ToString();
+            }
+            if (revs == s) // Checking whether string is palindrome or not  
+            {
+                Console.WriteLine("String is Palindrome \n Entered String Was {0} and reverse string is {1}", s, revs);
+            }
+            else
+            {
+                Console.WriteLine("String is not Palindrome \n Entered String Was {0} and reverse string is {1}", s, revs);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class playWithPalindromes
+    {
+
+    }
+}
---
+++ lec_3/object_practice/object_practice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace object_practice
+{
+    class Program
+    {
+        Random rand;
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            for (Console.WriteLine("loop 1"); Orange(); Console.WriteLine("loop 2"))
+            {
+
+                Console.WriteLine("iterations {0}", counter++);
+                Console.Read();
+
+                //   Console.WriteLine("Hello World!");
+            }
+        }
+
+            static bool Orange()
+        {
+                int counter = 0;
+                Console.WriteLine("i am orange method  {0}", counter++);
+            Console.Read();
+            Random rand = new Random();
+            int r = rand.Next(1, 100);
+            if(r>50)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
